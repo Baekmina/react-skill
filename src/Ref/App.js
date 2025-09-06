233e4168fafae1aab5fb7793e66139593207780c
백민아 <koyoa12@naver.com>
2025-09-06 17:31:35 +0900

    Ref App.js 수정

--- src/Ref/App.js
+++ src/Ref/App.js
@@ -1,20 +1,15 @@
 import { Component } from "react";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
-import IterationSample from "../Map/IterationSample";
-
 class App extends Component {
     render() {
-        return (
-        <IterationSample />
-        )
         // 컴포넌트에 ref 달기 (컴포넌트 내부에 있는 DOM을 컴포넌트 외부에서 사용할 때 씀)
-        // return (
-        //   <div>
-        //     <ScrollBox ref={(ref) => this.scrollBox=ref} />
-        //     <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
-        //   </div> 
-        // )
+        return (
+          <div>
+            <ScrollBox ref={(ref) => this.scrollBox=ref} />
+            <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+          </div> 
+        )
     }
 };
 
