f721acf13b017492958c958999cea11123a98d39
백민아 <koyoa12@naver.com>
2025-09-06 19:23:28 +0900

    useMemo 진행중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Counter from "./Hooks/useReducer/Counter";
 import Info from "./Hooks/useReducer/Info";
+import Average from "./Hooks/useMemo/Average";
 
 const App = () => {
 	return (
-		<Info />
+		<Average />
 	)
 }
 
