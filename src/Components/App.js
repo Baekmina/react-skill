203e86bb63c64271de571253674e4cd3bdd888a9
백민아 <koyoa12@naver.com>
2025-09-06 17:29:39 +0900

    Components App.js 파일 생성 및 이동

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import MyComponent from "./Components/MyComponent"
+import Counter from "./Components/Counter";
+import Say from "./Components/Say";
+
+const App = () => {
+    return <Say />
+    // return <Counter />
+    // return <MyComponent name="React">리액트</MyComponent>;
+};
+
+export default App;
