28e603e0e636ab485afbbf88992513c706cedf74
Baekmina <koyoa12@naver.com>
2025-09-09 23:23:34 +0900

    todo-app project 진행중

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import { Component } from "react";
-import SassComponent from "./Styling/SASS/SassComponent";
-import CSSModule from "./Styling/CSSModule/CSSModule";
-import StyledComponent from "./Styling/StyledComponents/StyledComponent";
+import { useState } from "react";
+import TodoInsert from "./Project/todo-app/components/TodoInsert/TodoInsert";
+import TodoList from "./Project/todo-app/components/TodoList/TodoList";
+import TodoTemplate from "./Project/todo-app/components/TodoTemplate/TodoTemplate";
 
-class App extends Component {
-	render() {
-		return (
-			<StyledComponent />
-			// <CSSModule />
-			// <SassComponent />
-		)
-	}
+const App = () => {
+	const [todos, setTodos] = useState([
+		{
+			id: 1,
+			text: '리액트의 기초 알아보기',
+			checked: true,
+		},
+		{
+			id: 2,
+			text: '컴포넌트 스타일링해 보기',
+			checked: true,
+		},
+		{
+			id: 3,
+			text: '일정 관리 앱 만들어 보기',
+			checked: false,
+		},
+	]);
+
+	return (
+		<TodoTemplate>
+			<TodoInsert />
+			<TodoList todos={todos} />
+		</TodoTemplate>
+	)
 }
-
-// const App = () => {
-// 	return (
-// 		<SassComponent />
-// 	)
-// }
 
 export default App;
