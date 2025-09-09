9b6bd717c0e12a98f16f444ceb405f8141ead0d9
Baekmina <koyoa12@naver.com>
2025-09-09 11:27:53 +0900

    useMemo hook 주석 추가

--- src/Hooks/useMemo/Average.js
+++ src/Hooks/useMemo/Average.js
@@ -2,9 +2,10 @@ import { useMemo, useState } from "react"
 
 const getAverage = (numbers) => {
     console.log('평균값 계산 중...');
-    if (numbers.length === 0) return 0;
+    if (numbers.length === 0) return 0; 
+    // return 0이 되는 순간 함수는 바로 종료됨. 따라서 다음에 오는 코드들은 실행되지 않음.
     const sum = numbers.reduce((a, b) => a + b);
-    return sum / numbers.length;
+    return sum / numbers.length; // 합계를 나누어서 평균을 구하는 공식
 }
 
 const Average = () => {
@@ -21,6 +22,7 @@ const Average = () => {
         setNumber('')
     }
 
+    // useMemo: 특정 계산 결과를 기억해두는 hook
     const avg = useMemo(() => getAverage(list), [list])
 
     return (
