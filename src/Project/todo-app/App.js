d0811fe3f733a3e9810f0b7235274b00656a3c2c
백민아 <koyoa12@naver.com>
2025-09-11 15:20:41 +0900

    project todo-app App.js 수정

--- src/Project/todo-app/App.js
+++ src/Project/todo-app/App.js
@@ -7,49 +7,52 @@ const App = () => {
 	const [todos, setTodos] = useState([
 		{
 			id: 1,
-			text: '리액트의 기초 알아보기',
+			text: "리액트의 기초 알아보기",
 			checked: true,
 		},
 		{
 			id: 2,
-			text: '컴포넌트 스타일링해 보기',
+			text: "컴포넌트 스타일링해 보기",
 			checked: true,
 		},
 		{
 			id: 3,
-			text: '일정 관리 앱 만들어 보기',
+			text: "일정 관리 앱 만들어 보기",
 			checked: false,
-		},
-	]);
+		}
+	])
 
 	const nextId = useRef(4);
 
-	const handleInsert = useCallback((text) => {
+	const onInsert = useCallback((text) => { // 새로운 todo의 내용(text)을 받는 것임.
 		const todo = {
 			id: nextId.current,
 			text,
 			checked: false,
 		}
-		setTodos(todos.concat(todo));
-		nextId.current += 1;
-	}, [todos]);
+		setTodos(todos.concat(todo)); // todos에 새로운 todo 객체 추가하기
+		nextId.current += 1; // id + 1 해주기
+	}, [todos])
 
-	const handleRemove = useCallback((id) => {
+	const onRemove = useCallback((id) => { // 삭제하고 싶은 항목 = id
+		// filter: 배열을 돌면서 true 값만 남김.
+		// todo.id가 id랑 같지 않은 경우 => ture => 배열에 남김
+		// todo.id가 id랑 같은 경우 => false => 배열에서 제거
 		setTodos(todos.filter(todo => todo.id !== id))
-	}, [todos]);
+	}, [todos])
 
-	const handleToggle = useCallback((id) => {
+	const onToggle = useCallback((id) => {
 		setTodos(
-			todos.map(
-				todo => todo.id === id ? {...todo, checked: !todo.checked} : todo
+			todos.map(todo =>
+				todo.id === id ? { ...todo, checked: !todo.checked } : todo,
 			)
 		)
-	}, [todos]);
+	}, [todos])
 
 	return (
 		<TodoTemplate>
-			<TodoInsert onInsert={handleInsert} />
-			<TodoList todos={todos} onRemove={handleRemove} onToggle={handleToggle} />
+			<TodoInsert onInsert={onInsert} />
+			<TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
 		</TodoTemplate>
 	)
 }
